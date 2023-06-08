7ac4eff208af90723f88d83d6490f1b272f7031d
Danish Hakim Khan <danish.hakim@gmail.com>
2023-06-08 12:25:40 +0500

    merge conflicts, changes related to contact form

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const { application } = require("express");
 const express = require("express");
-const nodemailer = require('nodemailer');
+const sendmail = require('sendmail');
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,95 +11,36 @@ app.get("/api", (req, res) => {
 });
 
 app.post("/contact", (req, res) => {
-    let transporter = nodemailer.createTransport({
-        host: 'smtp.ethereal.email',
-        port: 587,
-        secure: false,
-        auth: {
-            user: 'kristoffer78@ethereal.email',
-            pass: '3qBsadVVpeaK6dAw26'
-        },
-        tls: {
-            rejectUnauthorized: false
-        }
-    });
-
-    // verify connection configuration
-    transporter.verify(function(error, success) {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log("Server is ready to take our messages");
-        }
-    });
-
-    const output = `
-        <h3>Contact Details:</h3>
+    console.log(req);
+    const message = `
+        <p>Hello Sir,</p>
+        <p>${req.body.name} has sent a message from linumtech.com website, details are as below:</p>
         <ul>
         <li>Name: ${req.body.name}</li>
         <li>Email: ${req.body.email}</li>
+        <li>Phone: ${req.body.phone}</li>
+        <li>Inustry: ${req.body.industry}</li>
+        <li>Your Interest: ${req.body.interest}</li>
         </ul>
-        <h3>Message:</h3>
+        <p>Message:</p>
         <p>${req.body.message}</p>
     `;
-
-    let mailOptions = {
-        from: '"Nodemailer" <kristoffer78@ethereal.email>',
-        to: 'kristoffer78@ethereal.email',
-        subject: 'New message from Redux Co. contact form',
-        text: 'There is a new message',
-        html: output
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            reject(error)
-            return
-        }
-        resolve(info)
-    });
+    sendemail(message);
 });
+
+const sendemail = (emailmessage) => {
+    console.log('sending email');
+    sendmail({
+        from: 'info@linumtech.com',
+        to: 'danish.hakim@gmail.com',
+        subject: 'Contact Request from LinumTech Website',
+        html: emailmessage
+      }, function (err, reply) {
+        console.log(err && err.stack)
+        console.dir(reply)
+      })
+}
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
-
-const sendemail = (emailmessage) => {
-    let transporter = nodemailer.createTransport({
-        host: 'smtp.ethereal.email',
-        port: 587,
-        secure: false,
-        auth: {
-            user: 'kristoffer78@ethereal.email',
-            pass: '3qBsadVVpeaK6dAw26'
-        },
-        tls: {
-            rejectUnauthorized: false
-        }
-    });
-
-    // verify connection configuration
-    transporter.verify(function(error, success) {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log("Server is ready to take our messages");
-        }
-    });
-
-    let mailOptions = {
-        from: '"Nodemailer" <kristoffer78@ethereal.email>',
-        to: 'kristoffer78@ethereal.email',
-        subject: 'New message from Redux Co. contact form',
-        text: 'There is a new message',
-        html: emailmessage
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            reject(error)
-            return
-        }
-        resolve(info)
-    });
-}
